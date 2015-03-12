1d4a1f3d1d87d2a2b3ff277727491fb7ef37a962
Zoweb <me@zoweb.me>
2015-03-12 19:55:13 +1000

    Create example_currentserver.php

---
+++ example/example_currentserver.php
@@ -0,0 +1,12 @@
+<?php include '../includes/linkcounter.php'; ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Counter Example</title>
+</head>
+<body>
+<h1>Current Server Example</h1>
+<p>Page View Counter by Zoweb, 2015</p>
+</body>
+</html>
